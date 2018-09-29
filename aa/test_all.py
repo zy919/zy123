685a9804b372eb584fe58fa5fdc8314a5f6206b9
zy919 <viewjy919@163.com>
2018-09-29 21:03:05 +0800

    first commit

---
+++ aa/test_all.py
@@ -0,0 +1,19 @@
+import allure, pytest
+
+
+class Test_allure:
+    def setup(self):
+        pass
+
+    def teardown(self):
+        pass
+
+    @pytest.mark.parametrize("a", [1, 2, 3])
+    @pytest.allure.severity(pytest.allure.severity_level.CRITICAL)
+
+
+
+    @allure.step('我是测试步骤001')
+    def test_al(self, a):
+        allure.attach('描述', '我是测试步骤001的描述～～～')
+        assert a != 2
